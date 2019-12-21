28d75d25ccb007be1e26899d79a31ed5e276da84
jjmin321 <jjmin321@naver.com>
2019-12-21 10:41:06 +0900

    Python

---
+++ Python_Files/Studybook/calculator.py
@@ -0,0 +1,21 @@
+# calculator.py
+# 단축키
+# Command + '/' : 주석문 설정/해제
+# Tab : 들여쓰기
+# Shift + Tab : 들여쓰기 해제
+
+def add(a,b):
+    c = a + b
+    return c
+
+def subtract(a,b):
+    c = a-b
+    return c
+
+def multiply(a,b):
+    c = a * b
+    return c
+
+def divide(a,b):
+    c = a/b
+    return c
